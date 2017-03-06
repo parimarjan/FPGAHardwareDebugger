4c6815cdbbb5fbe6fac6f76a072c6722b6a6cd7a
Rishab <rishab@stanford.edu>
2017-03-05 22:12:50 -0800

    emptied

--- make.sh
+++
@@ -1,8 +0,0 @@
-./bake clean
-./bake
-sudo kextunload -b com.apple.driver.AppleUSBFTDI
-cd build
-make upload
-cd ..
-# upload the driver again so we can do the stuff with ports
-sudo kextload -b com.apple.driver.AppleUSBFTDI
