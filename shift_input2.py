7d26c28f12ae686a9ac7e3c9c7bea949bd3c807c
Parimarjan Negi <pnegi@stanford.edu>
2017-03-19 22:18:37 -0700

    Both receivers seem to be working

--- shift_input2.py
+++ shift_input2.py
@@ -25,33 +25,37 @@ counter = Counter(2, ce=True)
 wire(counter.CE, receiver.RECEIVED)
 
 dff1 = DFF(ce=True)
-# dff2 = DFF(ce=True)
+dff2 = DFF(ce=True)
 dff1(1)
-# dff2(1)
+dff2(1)
 
 enable1 = LUT2(~I0&I1)
 enable2 = LUT2(I0&~I1)
 
 enable1(counter.O[1], counter.O[0])
-# enable2(counter.O[0], counter.O[1])
+enable2(counter.O[1], counter.O[0])
 
 wire(dff1.CE, enable1.O)
-# wire(dff2.CE, enable2.O)
-
+wire(dff2.CE, enable2.O)
 
 not1 = Not()
 not1(dff1.O)
 
-# not2 = Not()
-# not2(dff2.O)
+not2 = Not()
+not2(dff2.O)
 
 receiver(main.CLKIN, main.RX, not1.O)
 
-# receiver2 = RECEIVER()
-# receiver2(main.CLKIN, main.RX, not2.O)
+receiver2 = RECEIVER()
+receiver2(main.CLKIN, main.RX, not2.O)
 
 echo = TRANSMITTER()
 echo(main.CLKIN, main.RX, receiver.REC_BYTE)
 wire(echo.TX, main.TX)
+
+wire(main.D1, receiver2.REC_BYTE[0])
+wire(main.D2, receiver2.REC_BYTE[3])
+wire(main.D3, receiver2.REC_BYTE[5])
+wire(main.D4, receiver2.REC_BYTE[7])
 
 compile(sys.argv[1], main)
